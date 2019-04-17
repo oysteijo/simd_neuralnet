c685215b12f52b0a349190453d047d4b805663dd
Oystein Schonning-Johansen <oysteijo@gmail.com>
2019-04-17 08:03:37 +0000

    Trying backprop in keras for reference

--- kerasmodel.py
+++ kerasmodel.py
@@ -4,12 +4,13 @@ import numpy as np
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+from keras.optimizers import SGD
 
 model = Sequential()
 model.add( Dense( 16, activation='sigmoid', input_dim=32 ) )
 model.add( Dense( 8, activation='sigmoid') )
 model.add( Dense( 4, activation='sigmoid') )
-model.compile( optimizer='SGD', loss="mean_squared_error")
+model.compile( optimizer=SGD(lr=0.1), loss="mean_squared_error")
 
 arr = np.load("initial_weights.npz")
 inp = np.load("random_input.npy")
@@ -20,3 +21,10 @@ model.set_weights(list(weights))
 
 
 print(model.predict(inp))
+
+train_sample = np.array([0.5,0.5,0.5,0.5], dtype=np.float32)
+train_sample = train_sample.reshape(1,4)
+model.train_on_batch(inp, train_sample )
+
+print(model.predict(inp))
+
