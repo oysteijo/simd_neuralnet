e2ad38f1e1e8b1c5594779366950ae2bc6890360
Oystein Schonning-Johansen <oysteijo@gmail.com>
2020-02-21 08:14:38 +0100

    Oh... I see trouble

--- optimizer/tests/test_sgd.c
+++ optimizer/tests/test_sgd.c
@@ -1,5 +1,5 @@
-#include "stochastic_gradient_descent.h"
-#include "c_npy.h"
+#include "optimizer_implementations.h"
+#include "npy_array.h"
 #include "neuralnet.h"
 #include "activation.h"
 #include "loss.h"
@@ -96,13 +96,13 @@ int main( int argc, char *argv[] )
     printf( "Momentum: %5.4f\n", momentum );
     printf( "Nesterov: %s\n", nesterov ? "True" : "False" );
 
-    cmatrix_t **train_test = c_npy_matrix_array_read( "mushroom_train.npz" );
-    if( !train_test ) return -1;
-
-    cmatrix_t *train_X = train_test[0];
-    cmatrix_t *train_Y = train_test[1];
-    cmatrix_t *test_X = train_test[2];
-    cmatrix_t *test_Y = train_test[3];
+    npy_array_list_t *filelist = npy_array_list_load( "mushroom_train.npz" );
+    
+    npy_array_list_t *iter = filelist;
+    npy_array_t *train_X = iter->array;  iter = iter->next;
+    npy_array_t *train_Y = iter->array;  iter = iter->next;
+    npy_array_t *test_X = iter->array;   iter = iter->next;
+    npy_array_t *test_Y = iter->array;
 
     /* if any of these asserts fails, try to open the weight in python and save with
      * np.ascontiguousarray( matrix ) */
@@ -119,9 +119,13 @@ int main( int argc, char *argv[] )
     printf( "train_Y shape: (%zu, %zu)\n", train_Y->shape[0], train_Y->shape[1] );
     printf( "test_X shape: (%zu, %zu)\n", test_X->shape[0], test_X->shape[1] );
     printf( "test_Y shape: (%zu, %zu)\n", test_Y->shape[0], test_Y->shape[1] );
-    /* It does not handle any whitespace in front of (or trailing) */
-    neuralnet_t *nn = neuralnet_new( "initial_mushroom_111_32_1.npz", (char*[]){ "relu", "sigmoid"} );
+
+    neuralnet_t *nn = neuralnet_create( 3,
+            INT_ARRAY( train_X->shape[1], 64, 32, 1 ),
+            STR_ARRAY( "relu", "relu,", "sigmoid" ) );
+
     assert( nn );
+    neuralnet_initialize( nn, "kaiming", "kaiming", "kaiming" );
     neuralnet_set_loss( nn, "binary_crossentropy" );
 
     optimizer_t *sgd = optimizer_new( nn, 
@@ -141,7 +145,6 @@ int main( int argc, char *argv[] )
 
     int n_epochs = 10;
     
-    float results[2*n_metrics];
     
     /* Table heading */
     printf("Epoch");
@@ -162,9 +165,10 @@ int main( int argc, char *argv[] )
     printf("\n");
     
     for ( int i = 0; i < n_epochs; i++ ){
+        float results[2*n_metrics];
         optimizer_run_epoch( sgd, n_train_samples, (float*) train_X->data, (float*) train_Y->data,
                                   n_test_samples, (float*) test_X->data, (float*) test_Y->data, results );
-        printf( "%4d ", i);  /* same leangth as "epoch" */
+        printf( "%4d ", i);  /* same length as "epoch" */
         for ( int j = 0; j < 2*n_metrics ; j++ )
             printf("%*.7e", longest_metric_name_len, results[j] );
         printf("\n");
@@ -174,6 +178,6 @@ int main( int argc, char *argv[] )
     neuralnet_save( nn, "after-%d-epochs.npz", n_epochs );
     neuralnet_free( nn );
     free( sgd );
-    c_npy_matrix_array_free( train_test );
+    npy_array_list_free( filelist );
     return 0;
 }    
